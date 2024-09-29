2223515cd2db5934c60e7bd852e4cf2f8f4f03ad
a7medsr <144508508+a7medsr@users.noreply.github.com>
2024-09-29 17:28:42 +0300

    Checkout view
    add

---
+++ Foodies/Controllers/checkoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foodies.Controllers
+{
+    public class checkoutController : Controller
+    {//checkout/checkout
+        public IActionResult checkout()
+        {
+            return View();
+        }
+    }
+}
